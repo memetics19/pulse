207ffcabf63e3628ef90cee33d6b0246be6d6d97
memetics19 <shreeda.bhat@yahoo.com>
2026-07-04 00:48:14 +0530

    chore(api): remove unused PULSE_ADMIN_TOKEN config

--- api/internal/config/config.go
+++ api/internal/config/config.go
@@ -3,7 +3,6 @@ package config
 import "os"
 
 type Config struct {
-	AdminToken      string
 	DatabaseURL     string
 	SQLitePath      string
 	ResendAPIKey    string
@@ -17,7 +16,6 @@ func Load() Config {
 		port = "8080"
 	}
 	return Config{
-		AdminToken:      os.Getenv("PULSE_ADMIN_TOKEN"),
 		DatabaseURL:     os.Getenv("DATABASE_URL"),
 		SQLitePath:      os.Getenv("SQLITE_PATH"),
 		ResendAPIKey:    os.Getenv("RESEND_API_KEY"),
